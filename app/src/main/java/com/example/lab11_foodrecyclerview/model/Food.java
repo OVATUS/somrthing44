0fcf164b97b9d95acc14d41b51adac905b9d4e61
OVATUS <nontapan.bo.66@ubu.ac.th>
2024-10-09 03:12:09 +0700

    sss

---
+++ app/src/main/java/com/example/lab11_foodrecyclerview/model/Food.java
@@ -0,0 +1,32 @@
+package com.example.lab11_foodrecyclerview.model;
+
+public class Food {
+    private String food_image;
+    private String food_name;
+    private int food_price;
+
+
+    public String getFood_image() {
+        return food_image;
+    }
+
+    public void setFood_image(String food_image) {
+        this.food_image = food_image;
+    }
+
+    public String getFood_name() {
+        return food_name;
+    }
+
+    public void setFood_name(String food_name) {
+        this.food_name = food_name;
+    }
+
+    public int getFood_price() {
+        return food_price;
+    }
+
+    public void setFood_price(int food_price) {
+        this.food_price = food_price;
+    }
+}
